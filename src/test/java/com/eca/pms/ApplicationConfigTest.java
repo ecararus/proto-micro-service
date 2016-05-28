3d091e3b293050a932e9ee27786a04d3374695e4
Cararus Eugeniu <cararuseugeniu@gmail.com>
2016-05-28 22:36:46 +0100

    add it

--- src/test/java/com/eca/pms/ApplicationConfigTest.java
+++ src/test/java/com/eca/pms/ApplicationConfigTest.java
@@ -1,8 +1,22 @@
 package com.eca.pms;
 
-
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(ApplicationConfig.class)
+@WebAppConfiguration()
+@DirtiesContext
 class ApplicationConfigTest {
+
+    @Test
+    public void applicationShouldStart_withGivenConfiguration() {
+    }
 
 }
